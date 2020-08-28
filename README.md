bab68dc4e9ce07d6bea87f1d72928df9618fdfc1
Lykrast <aymericpm@gmail.com>
2020-08-28 17:51:04 +0200

    Basic mod setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# GunsWithoutRoses
-Small Minecraft mod for simple Terraria-like guns
+# Guns Without Roses [![Curseforge](http://cf.way2muchnoise.eu/guns-without-roses.svg)](https://www.curseforge.com/minecraft/mc-mods/guns-without-roses) [![Curseforge](http://cf.way2muchnoise.eu/versions/guns-without-roses.svg)](https://www.curseforge.com/minecraft/mc-mods/guns-without-roses) [![Curseforge](http://cf.way2muchnoise.eu/packs/guns-without-roses.svg)](https://www.curseforge.com/minecraft/mc-mods/guns-without-roses)
+
+Adds some simple Terraria-like guns and bullets to go with them.
---
+++ src/main/java/lykrast/gunswithoutroses/GunsWithoutRoses.java
@@ -0,0 +1,32 @@
+package lykrast.gunswithoutroses;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod("gunswithoutroses")
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = GunsWithoutRoses.MODID)
+public class GunsWithoutRoses {
+	public static final String MODID = "gunswithoutroses";
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	public GunsWithoutRoses() {
+		//Configs one day
+	}
+
+	@SubscribeEvent
+	public static void registerItems(final RegistryEvent.Register<Item> event) {
+		LOGGER.info("Regsiter items");
+	}
+
+	@SubscribeEvent
+	public static void clientStuff(final FMLClientSetupEvent event) {
+		LOGGER.info("Client stuff");
+	}
+}
