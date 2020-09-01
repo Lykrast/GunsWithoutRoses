964deeb594e335fb094eacda00cd9de1306e86d0
Lykrast <aymericpm@gmail.com>
2020-09-01 02:08:17 +0200

    Functionally added Cherrounds

--- src/main/java/lykrast/gunswithoutroses/ModItems.java
+++ src/main/java/lykrast/gunswithoutroses/ModItems.java
@@ -10,7 +10,7 @@ import net.minecraftforge.registries.IForgeRegistry;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = GunsWithoutRoses.MODID)
 public class ModItems {
 	public static GunItem ironGun, goldGun, diamondShotgun, diamondSniper, diamondGatling;
-	public static BulletItem flintBullet, ironBullet, blazeBullet, hungerBullet;
+	public static BulletItem flintBullet, ironBullet, blazeBullet, hungerBullet, hanamiBullet;
 
 	@SubscribeEvent
 	public static void registerItems(final RegistryEvent.Register<Item> event) {
@@ -26,6 +26,8 @@ public class ModItems {
 		ironBullet = initItem(reg, new BulletItem(defP(), 6), "iron_bullet");
 		blazeBullet = initItem(reg, new BlazeBulletItem(defP(), 7), "blaze_bullet");
 		hungerBullet = initItem(reg, new HungerBulletItem(defP().maxStackSize(1), 5), "hunger_bullet");
+		
+		hanamiBullet = initItem(reg, new HanamiBulletItem(defP(), 4), "hanami_bullet");
 	}
 
 	public static Item.Properties defP() {
--- src/main/java/lykrast/gunswithoutroses/entity/BulletEntity.java
+++ src/main/java/lykrast/gunswithoutroses/entity/BulletEntity.java
@@ -11,12 +11,14 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.IndirectEntityDamageSource;
 import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
 
 public class BulletEntity extends AbstractFireballEntity {
 	private double damage = 1;
 	private boolean ignoreInvulnerability = false;
+	private double knockbackStrength = 0;
 	protected int ticksExisted;
 
 	public BulletEntity(EntityType<? extends BulletEntity> p_i50160_1_, World p_i50160_2_) {
@@ -37,7 +39,7 @@ public class BulletEntity extends AbstractFireballEntity {
 	}
 
 	private static final double STOP_TRESHOLD = 0.01;
-	
+
 	@Override
 	public void tick() {
 		if (ticksExisted > 200 || getMotion().lengthSquared() < STOP_TRESHOLD) {
@@ -57,8 +59,15 @@ public class BulletEntity extends AbstractFireballEntity {
 			int lastHurtResistant = target.hurtResistantTime;
 			if (ignoreInvulnerability) target.hurtResistantTime = 0;
 			boolean damaged = target.attackEntityFrom((new IndirectEntityDamageSource("arrow", this, shooter)).setProjectile(), (float) damage);
-			if (damaged && shooter instanceof LivingEntity) {
-				applyEnchantments((LivingEntity) shooter, target);
+			if (damaged && target instanceof LivingEntity) {
+				LivingEntity livingTarget = (LivingEntity)target;
+				if (knockbackStrength > 0) {
+					//TODO Hanami's Instability
+					Vector3d vector3d = this.getMotion().mul(1, 0, 1).normalize().scale(knockbackStrength);
+					if (vector3d.lengthSquared() > 0.0D) livingTarget.addVelocity(vector3d.x, 0.1D, vector3d.z);
+				}
+
+				if (shooter instanceof LivingEntity) applyEnchantments((LivingEntity)shooter, target);
 			}
 			else if (!damaged && ignoreInvulnerability) target.hurtResistantTime = lastHurtResistant;
 		}
@@ -75,6 +84,7 @@ public class BulletEntity extends AbstractFireballEntity {
 		super.writeAdditional(compound);
 		compound.putDouble("damage", damage);
 		compound.putBoolean("ignoreinv", ignoreInvulnerability);
+		compound.putDouble("knockback", knockbackStrength);
 	}
 
 	@Override
@@ -82,6 +92,7 @@ public class BulletEntity extends AbstractFireballEntity {
 		super.readAdditional(compound);
 		damage = compound.getDouble("damage");
 		ignoreInvulnerability = compound.getBoolean("ignoreinv");
+		knockbackStrength = compound.getDouble("knockback");
 	}
 
 	public void setDamage(double damage) {
@@ -94,6 +105,10 @@ public class BulletEntity extends AbstractFireballEntity {
 
 	public void setIgnoreInvulnerability(boolean ignoreInvulnerability) {
 		this.ignoreInvulnerability = ignoreInvulnerability;
+	}
+
+	public void setKnockbackStrength(double knockbackStrength) {
+		this.knockbackStrength = knockbackStrength;
 	}
 
 	@Override
