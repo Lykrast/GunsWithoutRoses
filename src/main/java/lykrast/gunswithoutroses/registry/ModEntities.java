fa0c7adf67d6d7806fc1997bacebef3468c6400d
Lykrast <aymericpm@gmail.com>
2022-03-06 00:26:40 +0100

    Ducktaped it back

--- src/main/java/lykrast/gunswithoutroses/registry/ModEntities.java
+++ src/main/java/lykrast/gunswithoutroses/registry/ModEntities.java
@@ -11,17 +11,18 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = GunsWithoutRoses.MODID)
 public class ModEntities {
-	//So it doesn't like doing the build() in the static initializer now...
+	//Fuck it I'm copying Nature's Aura
+	//There's a lot of ducktape in there cause it's not how the file looked like originally
 	public static EntityType<BulletEntity> BULLET;
 
+	@SuppressWarnings("unchecked")
 	@SubscribeEvent
 	public static void regsiterEntities(final RegistryEvent.Register<EntityType<?>> event) {
 		IForgeRegistry<EntityType<?>> reg = event.getRegistry();
-		//So I'll ducktape it here by just moving the initializer and making it not final
-		BULLET = EntityType.Builder
+		reg.register(EntityType.Builder
 				.<BulletEntity>of(BulletEntity::new, MobCategory.MISC)
 				.sized(0.3125f, 0.3125f).setUpdateInterval(10).setTrackingRange(64).setShouldReceiveVelocityUpdates(true)
-				.build(GunsWithoutRoses.MODID + ":bullet");
-		BULLET.setRegistryName(GunsWithoutRoses.MODID, "bullet");
+				.build(GunsWithoutRoses.MODID + ":bullet").setRegistryName(GunsWithoutRoses.MODID, "bullet"));
+		BULLET = (EntityType<BulletEntity>) reg.getValue(GunsWithoutRoses.rl("bullet"));
 	}
 }
