4f4ef8a192544685e0310676f4f8e2b460d5db01
HypherionMC <hypherionmc@gmail.com>
2022-03-26 17:57:33 +0200

    Added new guns (Needing models, textures and sounds)

--- src/main/java/lykrast/gunswithoutroses/config/GunsWithoutRosesConfig.java
+++ src/main/java/lykrast/gunswithoutroses/config/GunsWithoutRosesConfig.java
@@ -21,8 +21,23 @@ public class GunsWithoutRosesConfig {
     public static ForgeConfigSpec.DoubleValue diamondShotgunDamageMultiplier;
     public static ForgeConfigSpec.IntValue diamondShotgunFireDelay;
 
+    public static ForgeConfigSpec.DoubleValue diamondDoubleShotgunDamageMultiplier;
+    public static ForgeConfigSpec.IntValue diamondDoubleShotgunFireDelay;
+
     public static ForgeConfigSpec.DoubleValue diamondSniperDamageMultiplier;
     public static ForgeConfigSpec.IntValue diamondSniperFireDelay;
+
+    public static ForgeConfigSpec.DoubleValue diamondCarbineDamageMultiplier;
+    public static ForgeConfigSpec.IntValue diamondCarbineFireDelay;
+
+    public static ForgeConfigSpec.DoubleValue diamondSmgDamageMultiplier;
+    public static ForgeConfigSpec.IntValue diamondSmgFireDelay;
+
+    public static ForgeConfigSpec.DoubleValue diamondAssaultDamageMultiplier;
+    public static ForgeConfigSpec.IntValue diamondAssaultFireDelay;
+
+    public static ForgeConfigSpec.DoubleValue diamondStreamGatlingDamageMultiplier;
+    public static ForgeConfigSpec.IntValue diamondStreamGatlingFireDelay;
 
     public static ForgeConfigSpec.DoubleValue diamondGatlingDamageMultiplier;
     public static ForgeConfigSpec.IntValue diamondGatlingFireDelay;
@@ -43,13 +58,13 @@ public class GunsWithoutRosesConfig {
                 .defineInRange("fireDelayModifier", 0.2D, 0D, 1.0D);
         builder.pop();
 
-        builder.push("iron_gun");
+        builder.push("pistol");
         ironDamageMultiplier = builder
-                .comment("Define the Damage multiplier for Iron guns")
+                .comment("Define the Damage multiplier for Pistols")
                 .defineInRange("ironDamageModifier", 1D, 0.5D, 1.5D);
         ironFireDelay = builder
-                .comment("Configure the Fire delay for Iron Guns")
-                .defineInRange("ironFireDelay", 16, 0, 48);
+                .comment("Configure the Fire delay for Pistols")
+                .defineInRange("ironFireDelay", 12, 0, 48);
         builder.pop();
 
         builder.push("gold_gun");
@@ -61,7 +76,7 @@ public class GunsWithoutRosesConfig {
                 .defineInRange("goldFireDelay", 28, 0, 48);
         builder.pop();
 
-        builder.push("diamond_shotgun");
+        builder.push("diamond_weapons");
         diamondShotgunDamageMultiplier = builder
                 .comment("Define the Damage multiplier for Diamond Shotguns")
                 .defineInRange("diamondShotgunDamageMultiplier", 0.5D, 0.5D, 1.5D);
@@ -69,12 +84,47 @@ public class GunsWithoutRosesConfig {
                 .comment("Configure the Fire delay for Diamond Shotguns")
                 .defineInRange("diamondShotgunFireDelay", 24, 0, 48);
 
+        diamondDoubleShotgunDamageMultiplier = builder
+                .comment("Define the Damage multiplier for Diamond Shotguns")
+                .defineInRange("diamondDoubleShotgunDamageMultiplier", 0.75D, 0.5D, 1.5D);
+        diamondDoubleShotgunFireDelay = builder
+                .comment("Configure the Fire delay for Diamond Shotguns")
+                .defineInRange("diamondDoubleShotgunFireDelay", 60, 0, 48);
+
         diamondSniperDamageMultiplier = builder
                 .comment("Define the Damage multiplier for Diamond Snipers")
                 .defineInRange("diamondSniperDamageMultiplier", 2.5D, 0.5D, 2.5D);
         diamondSniperFireDelay = builder
                 .comment("Configure the Fire delay for Diamond Snipers")
                 .defineInRange("diamondSniperFireDelay", 48, 0, 48);
+
+        diamondCarbineDamageMultiplier = builder
+                .comment("Define the Damage multiplier for Diamond Carbine")
+                .defineInRange("diamondCarbineDamageMultiplier", 1.5D, 0.5D, 2.5D);
+        diamondCarbineFireDelay = builder
+                .comment("Configure the Fire delay for Diamond Carbine")
+                .defineInRange("diamondCarbineFireDelay", 28, 0, 48);
+
+        diamondSmgDamageMultiplier = builder
+                .comment("Define the Damage multiplier for Diamond SMG")
+                .defineInRange("diamondSmgDamageMultiplier", 0.75D, 0.5D, 2.5D);
+        diamondSmgFireDelay = builder
+                .comment("Configure the Fire delay for Diamond SMG")
+                .defineInRange("diamondSmgFireDelay", 4, 0, 48);
+
+        diamondAssaultDamageMultiplier = builder
+                .comment("Define the Damage multiplier for Diamond SMG")
+                .defineInRange("diamondAssaultDamageMultiplier", 0.75D, 0.5D, 2.5D);
+        diamondAssaultFireDelay = builder
+                .comment("Configure the Fire delay for Diamond SMG")
+                .defineInRange("diamondAssaultFireDelay", 6, 0, 48);
+
+        diamondStreamGatlingDamageMultiplier = builder
+                .comment("Define the Damage multiplier for Diamond SMG")
+                .defineInRange("diamondStreamGatlingDamageMultiplier", 1D, 0.5D, 2.5D);
+        diamondStreamGatlingFireDelay = builder
+                .comment("Configure the Fire delay for Diamond SMG")
+                .defineInRange("diamondStreamGatlingFireDelay", 4, 0, 48);
 
         diamondGatlingDamageMultiplier = builder
                 .comment("Define the Damage multiplier for Diamond Gatlings")
--- src/main/java/lykrast/gunswithoutroses/registry/ModItems.java
+++ src/main/java/lykrast/gunswithoutroses/registry/ModItems.java
@@ -15,19 +15,43 @@ import net.minecraftforge.registries.IForgeRegistry;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = GunsWithoutRoses.MODID)
 public class ModItems {
 
-	public static GunItem ironGun, goldGun, diamondShotgun, diamondSniper, diamondGatling;
+	public static GunItem
+			ironGun,
+			goldGun,
+			diamondShotgun,
+			doubleBarrelShotgun,
+			carbineSniper,
+			diamondSniper,
+			diamondGatling,
+			smgGatling,
+			streamGatling,
+			assaultGatling;
+
 	public static BulletItem flintBullet, ironBullet, blazeBullet, hungerBullet;
 
 	@SubscribeEvent
 	public static void registerItems(final RegistryEvent.Register<Item> event) {
 		IForgeRegistry<Item> reg = event.getRegistry();
 
-		ironGun = initItem(reg, new GunItem(defP().durability(513), 0, GunsWithoutRosesConfig.ironDamageMultiplier.get(), GunsWithoutRosesConfig.ironFireDelay.get(), 1.5, 14).repair(() -> Ingredient.of(Tags.Items.INGOTS_IRON)), "iron_gun");
-		goldGun = initItem(reg, new GunItem(defP().durability(104), 0, GunsWithoutRosesConfig.goldDamageMultiplier.get(), GunsWithoutRosesConfig.goldFireDelay.get(), 1.5, 22).repair(() -> Ingredient.of(Tags.Items.INGOTS_GOLD)), "gold_gun");
-		diamondShotgun = initItem(reg, new ShotgunItem(defP().durability(2076), 0, GunsWithoutRosesConfig.diamondShotgunDamageMultiplier.get(), GunsWithoutRosesConfig.diamondShotgunFireDelay.get(), 6, 10, 5).ignoreInvulnerability(true).fireSound(ModSounds.shotgun).repair(() -> Ingredient.of(Tags.Items.GEMS_DIAMOND)), "diamond_shotgun");
-		diamondSniper = initItem(reg, new GunItem(defP().durability(2076), 0, GunsWithoutRosesConfig.diamondSniperDamageMultiplier.get(), GunsWithoutRosesConfig.diamondSniperFireDelay.get(), 0, 10).projectileSpeed(4).fireSound(ModSounds.sniper).repair(() -> Ingredient.of(Tags.Items.GEMS_DIAMOND)), "diamond_sniper");
-		diamondGatling = initItem(reg, new GatlingItem(defP().durability(2076), 0, 0.75, 4, 4, 10).ignoreInvulnerability(true).repair(() -> Ingredient.of(Tags.Items.GEMS_DIAMOND)), "diamond_gatling");
+		// Pistols
+		ironGun = initItem(reg, new GunItem(defP().durability(513), 0, GunsWithoutRosesConfig.ironDamageMultiplier.get(), GunsWithoutRosesConfig.ironFireDelay.get(), 5, 14).repair(() -> Ingredient.of(Tags.Items.INGOTS_IRON)), "iron_gun").projectileSpeed(4);
+		goldGun = initItem(reg, new GunItem(defP().durability(104), 0, GunsWithoutRosesConfig.goldDamageMultiplier.get(), GunsWithoutRosesConfig.goldFireDelay.get(), 5, 22).repair(() -> Ingredient.of(Tags.Items.INGOTS_GOLD)), "gold_gun").projectileSpeed(4);
 
+		// Shotguns
+		diamondShotgun = initItem(reg, new ShotgunItem(defP().durability(2076), 0, GunsWithoutRosesConfig.diamondShotgunDamageMultiplier.get(), GunsWithoutRosesConfig.diamondShotgunFireDelay.get(), 6, 10, 5).ignoreInvulnerability(true).fireSound(ModSounds.shotgun).repair(() -> Ingredient.of(Tags.Items.GEMS_DIAMOND)), "diamond_shotgun").projectileSpeed(16);
+		doubleBarrelShotgun = initItem(reg, new ShotgunItem(defP().durability(2076), 0, GunsWithoutRosesConfig.diamondDoubleShotgunDamageMultiplier.get(), GunsWithoutRosesConfig.diamondDoubleShotgunFireDelay.get(), 20, 10, 10).ignoreInvulnerability(true).fireSound(ModSounds.shotgun).repair(() -> Ingredient.of(Tags.Items.GEMS_DIAMOND)), "double_barrel_shotgun").projectileSpeed(16);
+
+		// Snipers
+		diamondSniper = initItem(reg, new GunItem(defP().durability(2076), 0, GunsWithoutRosesConfig.diamondSniperDamageMultiplier.get(), GunsWithoutRosesConfig.diamondSniperFireDelay.get(), 0, 10).projectileSpeed(32).fireSound(ModSounds.sniper).repair(() -> Ingredient.of(Tags.Items.GEMS_DIAMOND)), "diamond_sniper");
+		carbineSniper = initItem(reg, new GunItem(defP().durability(2076), 0, GunsWithoutRosesConfig.diamondCarbineDamageMultiplier.get(), GunsWithoutRosesConfig.diamondCarbineFireDelay.get(), 0, 10).projectileSpeed(32).fireSound(ModSounds.sniper).repair(() -> Ingredient.of(Tags.Items.GEMS_DIAMOND)), "carbine_sniper");
+
+		// Gatlings
+		diamondGatling = initItem(reg, new GatlingItem(defP().durability(2076), 0, GunsWithoutRosesConfig.diamondGatlingDamageMultiplier.get(), GunsWithoutRosesConfig.diamondGatlingFireDelay.get(), 4, 10).ignoreInvulnerability(true).repair(() -> Ingredient.of(Tags.Items.GEMS_DIAMOND)), "diamond_gatling").projectileSpeed(16);
+		smgGatling = initItem(reg, new GatlingItem(defP().durability(2076), 0, GunsWithoutRosesConfig.diamondSmgDamageMultiplier.get(), GunsWithoutRosesConfig.diamondSmgFireDelay.get(), 15, 10).ignoreInvulnerability(true).repair(() -> Ingredient.of(Tags.Items.GEMS_DIAMOND)), "smg_gatling").projectileSpeed(16);
+		streamGatling = initItem(reg, new GatlingItem(defP().durability(2076), 0, GunsWithoutRosesConfig.diamondStreamGatlingDamageMultiplier.get(), GunsWithoutRosesConfig.diamondStreamGatlingFireDelay.get(), 5, 10).ignoreInvulnerability(true).repair(() -> Ingredient.of(Tags.Items.GEMS_DIAMOND)), "stream_gatling").projectileSpeed(3);
+		assaultGatling = initItem(reg, new GatlingItem(defP().durability(2076), 0, GunsWithoutRosesConfig.diamondAssaultDamageMultiplier.get(), GunsWithoutRosesConfig.diamondAssaultFireDelay.get(), 10, 10).ignoreInvulnerability(true).repair(() -> Ingredient.of(Tags.Items.GEMS_DIAMOND)), "assault_gatling").projectileSpeed(16);
+
+        // Bullets
 		flintBullet = initItem(reg, new BulletItem(defP(), GunsWithoutRosesConfig.flintBulletDamage.get()), "flint_bullet");
 		ironBullet = initItem(reg, new BulletItem(defP(), GunsWithoutRosesConfig.ironBulletDamage.get()), "iron_bullet");
 		blazeBullet = initItem(reg, new BlazeBulletItem(defP(), GunsWithoutRosesConfig.blazeBulletDamage.get()), "blaze_bullet");
