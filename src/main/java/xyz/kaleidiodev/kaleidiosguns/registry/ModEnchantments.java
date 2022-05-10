311c4ce16dd88ac716c02d94b09bc9dd31a56c44
Kaleidio <christhefennec@gmail.com>
2022-05-10 14:20:45 +0100

    rebalance enchantment rarities and costs

--- src/main/java/xyz/kaleidiodev/kaleidiosguns/registry/ModEnchantments.java
+++ src/main/java/xyz/kaleidiodev/kaleidiosguns/registry/ModEnchantments.java
@@ -20,9 +20,9 @@ public class ModEnchantments {
 	public static void registerEnchantments(final RegistryEvent.Register<Enchantment> event) {
 		IForgeRegistry<Enchantment> reg = event.getRegistry();
 
-		impact = initEnchant(reg, new GunEnchantment(Enchantment.Rarity.COMMON, 5, 1, 11, 20), "impact");
+		impact = initEnchant(reg, new GunEnchantment(Enchantment.Rarity.COMMON, 5, 3, 11, 20), "impact");
 		bullseye = initEnchant(reg, new GunEnchantment(Enchantment.Rarity.COMMON, 3, 5, 9, 15), "bullseye");
-		sleightOfHand = initEnchant(reg, new GunEnchantment(Enchantment.Rarity.UNCOMMON, 2, 12, 20, 40), "sleight_of_hand");
+		sleightOfHand = initEnchant(reg, new GunEnchantment(Enchantment.Rarity.RARE, 2, 12, 20, 40), "sleight_of_hand");
 		preserving = initEnchant(reg, new GunEnchantment(Enchantment.Rarity.RARE, 3, 15, 9, 50), "preserving");
 		accelerator = initEnchant(reg, new GunEnchantment(Enchantment.Rarity.UNCOMMON, 3, 10, 15, 35), "accelerator");
 	}
