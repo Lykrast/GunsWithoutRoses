03ec7d2d645cf85732cf829887b469fb5ae316fe
Kaleidio <christhefennec@gmail.com>
2022-06-05 22:10:49 +0100

    put enchanted books into the correct tab

--- src/main/java/xyz/kaleidiodev/kaleidiosguns/registry/ItemGroupGuns.java
+++ src/main/java/xyz/kaleidiodev/kaleidiosguns/registry/ItemGroupGuns.java
@@ -1,11 +1,14 @@
 package xyz.kaleidiodev.kaleidiosguns.registry;
 
+import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 
+import javax.annotation.Nullable;
+
 public class ItemGroupGuns extends ItemGroup {
 
-	public static final ItemGroup INSTANCE = new ItemGroupGuns(ItemGroup.getGroupCountSafe(), "kaleidiosguns");
+	public static final ItemGroup INSTANCE = new ItemGroupGuns(ItemGroup.getGroupCountSafe(), "kaleidiosguns").setEnchantmentCategories(ModEnchantments.TYPE_GUN);
 
 	public ItemGroupGuns(int index, String label) {
 		super(index, label);
