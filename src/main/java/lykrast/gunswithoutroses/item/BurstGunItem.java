94e5d9e5d379fcc82fee7d1fe3bf5dde2e6383ba
Lykrast <aymericpm@gmail.com>
2025-07-25 16:58:45 +0200

    Burst gun adjustments

--- src/main/java/lykrast/gunswithoutroses/item/BurstGunItem.java
+++ src/main/java/lykrast/gunswithoutroses/item/BurstGunItem.java
@@ -20,11 +20,17 @@ import net.minecraft.world.level.Level;
 public class BurstGunItem extends GunItem {
 	//Unused in base GWR but want this for future addons and want to ensure consistency
 
-	protected int burstSize;
+	protected int burstSize, burstFireDelay;
 
 	public BurstGunItem(Properties properties, int bonusDamage, double damageMultiplier, int fireDelay, double inaccuracy, int enchantability, int burstSize) {
 		super(properties, bonusDamage, damageMultiplier, fireDelay, inaccuracy, enchantability);
 		this.burstSize = burstSize;
+		burstFireDelay = 1;
+	}
+	
+	public BurstGunItem burstFireDelay(int burstFireDelay) {
+		this.burstFireDelay = Math.max(1, burstFireDelay);
+		return this;
 	}
 
 	@Override
@@ -44,6 +50,11 @@ public class BurstGunItem extends GunItem {
 	public void onUseTick(Level world, LivingEntity user, ItemStack gun, int ticks) {
 		//I'm copying that part a lot, maybe I need to extract it
 		if (user instanceof Player) {
+			//this goes from 0 to useduration-1
+			//with a fire delay of n, we'd want to fire on 0, n, 2n... and so end on (burstSize-1)*burstFireDelay
+			int used = getUseDuration(gun) - ticks;
+			if (used % burstFireDelay != 0) return;
+			
 			Player player = (Player) user;
 			ItemStack ammo = player.getProjectile(gun);
 			if (!ammo.isEmpty() || player.getAbilities().instabuild) {
@@ -92,17 +103,18 @@ public class BurstGunItem extends GunItem {
 	@Override
 	protected void customFireRateTooltip(ItemStack stack, @Nullable Level world, List<Component> tooltip) {
 		//burst size
+		//TODO find a way to display the burst fire delay
 		int burstSize = getBurstSize(stack);
 		tooltip.add(Component.translatable("tooltip.gunswithoutroses.burst.size", Component.literal(Integer.toString(burstSize)).withStyle(ChatFormatting.WHITE))
 				.withStyle(isBurstSizeModified(stack) ? ChatFormatting.LIGHT_PURPLE : ChatFormatting.DARK_GREEN));
 
 		//fire rate
-		//holding down right click with a burstsize of N syncs up with N+?+firedelay
+		//holding down right click with a useduration of N syncs up with N+?+firedelay
 		//that is +0~2 and seems to be consistent with the same burstsize and firedelay pairs
 		//+1 and +2 seem to most common so I'm going with +1, I doubt anyone is gonna metronome it
 		int fireDelay = getFireDelay(stack, null);
 		MutableComponent values = Component.translatable("tooltip.gunswithoutroses.gun.firerate.values", Component.literal(Integer.toString(fireDelay)).withStyle(ChatFormatting.WHITE),
-				Component.literal(Integer.toString((60 * 20 * burstSize) / (fireDelay + burstSize + 1))).withStyle(ChatFormatting.WHITE)).withStyle(ChatFormatting.GRAY);
+				Component.literal(Integer.toString((60 * 20 * burstSize) / (fireDelay + getUseDuration(stack) + 1))).withStyle(ChatFormatting.WHITE)).withStyle(ChatFormatting.GRAY);
 		tooltip.add(Component.translatable("tooltip.gunswithoutroses.burst.firerate", values).withStyle(isFireDelayModified(stack) ? ChatFormatting.LIGHT_PURPLE : ChatFormatting.DARK_GREEN));
 	}
 	
@@ -118,7 +130,7 @@ public class BurstGunItem extends GunItem {
 
 	@Override
 	public int getUseDuration(ItemStack stack) {
-		return getBurstSize(stack);
+		return (getBurstSize(stack)-1)*burstFireDelay+1;
 	}
 
 	@Override
